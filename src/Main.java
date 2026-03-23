1c557449b953e207c4857e674a7272858204aaa4
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2026-03-23 10:28:28 -0300

    feat: Testar os controles de repetição while e for para o sistema de mega sena

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
+        Random generate = new Random();
+
+//        int i = 0;
+//        while(i < 6){
+//            int number = generate.nextInt(60);
+//            System.out.println(number);
+//            i++ ;
+
+
+        for(int i = 0; i < 6; i++){
+            int number = generate.nextInt(100);
+            System.out.println(number);
+        }
     }
+
 }
